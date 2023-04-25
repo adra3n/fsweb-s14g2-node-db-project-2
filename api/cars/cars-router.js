bf9b1df63c94a802e699d571b9f6fab753da3d63
Sertaç Kocagil <srtc.kcgl@gmail.com>
2023-04-25 23:45:07 +0300

    wip

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,38 @@
 // HOKUS POKUS
+const router = require('express').Router()
+const carsModel = require('./cars-model')
+const middleware = require('./cars-middleware')
+
+router.get('/', async (req, res, next) => {
+  try {
+    const allCars = await carsModel.getAll()
+    res.json(allCars)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/:id', middleware.checkCarId, async (req, res, next) => {
+  try {
+    res.json(req.existCar)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post(
+  '/',
+  middleware.checkCarPayload,
+  middleware.checkVinNumberValid,
+  middleware.checkVinNumberUnique,
+  async (req, res, next) => {
+    try {
+      const createdCar = await carsModel.create(req.body)
+      res.status(201).json(createdCar)
+    } catch (error) {
+      next(error)
+    }
+  }
+)
+
+module.exports = router
