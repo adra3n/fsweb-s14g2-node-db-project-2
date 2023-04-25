dd7c4c61ad5d8c3f163b8cec903044428bd0075e
Sertaç Kocagil <srtc.kcgl@gmail.com>
2023-04-25 21:47:56 +0300

    wip

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,32 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
   // HOKUS POKUS
+  return db('cars')
 }
 
 const getById = () => {
-  // HOKUS POKUS
+  // HOKUS POKUS+
+  return db('cars').where('id', id).first()
 }
 
 const create = () => {
   // HOKUS POKUS
+  const insertedCar = db('cars')
+    .insert(cars)
+    .then((ids) => {
+      return getById(ids[0])
+    })
+  return insertedCar
+}
+
+const getByVin = (vin) => {
+  return db('cars').where('vin', vin).first()
+}
+
+module.exports = {
+  getAll,
+  create,
+  getById,
+  getByVin,
 }
